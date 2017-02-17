22ffe3d6b393b2ea1152f5a42a82933578bac49a
Jason Banich <Jdban101@gmail.com>
2017-02-17 12:50:07 -0800

    mess

---
+++ python/ppp.py
@@ -0,0 +1,32 @@
+# from typing import Dict
+import sys
+import re
+
+
+def my_func(numz, strz):  # type: (int, str) -> None
+    numz *= 5
+    strz += "str"
+    num *= strz
+    return numz > 10
+
+
+print "hello"
+
+stra = "5"
+
+num = 2
+
+result = stra + num
+
+my_list = []
+
+my_list.append(5)
+
+my_list.append("2")
+
+none_res = my_func(5, "10")
+
+none_res = my_func("5", 10)
+
+some_num = 5 * my_func(5, "5")
+
