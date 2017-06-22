7582b0e854b6a71073c14e758d401a105a322cb8
Jason Banich <Jdban101@gmail.com>
2017-06-21 20:53:07 -0700

    Initial commit of joke module

---
+++ python_packages/joke_module/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='joke_module',
+    version='0.1.0',
+    description='Test description',
+    url='http://github.com/jdban/git-playground/python_packages/joke_module',
+    author='Jason B',
+    author_email='fake@example.com',
+    license='MIT',
+    packages=['joke_module'],
+    zip_safe=False)
